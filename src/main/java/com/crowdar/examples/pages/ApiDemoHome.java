8186222393b672ccce05ef6b559eef098093a040
Pole <juanmspoleti@gmail.com>
2019-12-24 15:54:49 -0300

    update a version 1.9.0 Lippia

--- src/main/java/com/crowdar/examples/pages/ApiDemoHome.java
+++ src/main/java/com/crowdar/examples/pages/ApiDemoHome.java
@@ -1,29 +1,31 @@
 package com.crowdar.examples.pages;
 
+import com.crowdar.core.pageObjects.PageBaseMobile;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import com.crowdar.bdd.cukes.SharedDriver;
-import com.crowdar.mobile.core.PageBase;
+public class ApiDemoHome extends PageBaseMobile {
 
-public class ApiDemoHome extends PageBase{
+    private final String ACTION_BAR_XPATH = "//*[@resource-id='android:id/action_bar']";
+    private final String TEXT_VIEW_XPATH = "//android.widget.TextView";
 
-	public ApiDemoHome(SharedDriver driver){
-		super(driver);
-		this.url = "";
-	}
-	
-    public String getTitle(){
-    	return getMobileElement(By.xpath("//*[@resource-id='android:id/action_bar']")).findElement(By.xpath("//android.widget.TextView")).getText();
+    public ApiDemoHome(RemoteWebDriver driver) {
+        super(driver);
+        this.url = "";
     }
 
-    private WebElement getMenuElement(String menu){
-    	return getMobileElement(By.xpath("//android.widget.TextView[@content-desc="+"\""+menu+"\""+"]"));
+    public String getTitle() {
+        return getWebElement(By.xpath(ACTION_BAR_XPATH)).findElement(By.xpath(TEXT_VIEW_XPATH)).getText();
     }
-    
-   public void clickMenuElement(String menu){
-       WebElement element = getMenuElement(menu);
-        element.click();
-   }
+
+    private WebElement getMenuElement(String menu) {
+        return getWebElement(By.xpath("//android.widget.TextView[@content-desc=" + "\"" + menu + "\"" + "]"));
+    }
+
+    public void clickMenuElement(String menu) {
+        WebElement element = getMenuElement(menu);
+        clickElement(element);
+    }
 
 }
--- src/main/java/com/crowdar/examples/steps/ApkApiDemoSteps.java
+++ src/main/java/com/crowdar/examples/steps/ApkApiDemoSteps.java
@@ -1,37 +1,28 @@
 package com.crowdar.examples.steps;
 
-import org.testng.Assert;
-
-import com.crowdar.bdd.cukes.SharedDriver;
+import com.crowdar.core.Injector;
 import com.crowdar.core.PageSteps;
 import com.crowdar.examples.pages.ApiDemoHome;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.testng.Assert;
 
 public class ApkApiDemoSteps extends PageSteps {
 
-    private ApiDemoHome apiDemoHome;
-
-    public ApkApiDemoSteps(SharedDriver driver){
-        super(driver);
-        apiDemoHome = new ApiDemoHome(driver);
-    }
-
     @Given("The user opens the '(.*)' application")
-    public void openAplication(String title){
-    	Assert.assertEquals(apiDemoHome.getTitle(), title);
+    public void openAplication(String title) {
+        Assert.assertEquals(Injector._page(ApiDemoHome.class).getTitle(), title);
     }
 
     @When("The user clicks over '(.*)' menu element")
-    public void executeAction(String menuElementName){
-        apiDemoHome.clickMenuElement(menuElementName);
+    public void executeAction(String menuElementName) {
+        Injector._page(ApiDemoHome.class).clickMenuElement(menuElementName);
     }
 
     @Then("The user sees the application '(.*)' open")
-    public void openAssertion(String title) throws InterruptedException{
-    	Thread.sleep(1500l);
-    	Assert.assertEquals(apiDemoHome.getTitle(), title);
+    public void openAssertion(String title) throws InterruptedException {
+        Thread.sleep(1500l);
+        Assert.assertEquals(Injector._page(ApiDemoHome.class).getTitle(), title);
     }
 }
